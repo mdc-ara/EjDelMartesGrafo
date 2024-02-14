12a4ba923fe704c1c3417646a7e16b8ee255e4a9
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-02-14 13:55:43 +0100

    Primer commit

---
+++ src/main/java/es/medac/ejdelmartesgrafo/EjDelMartesGrafo.java
@@ -0,0 +1,13 @@
+
+package es.medac.ejdelmartesgrafo;
+
+/**
+ *
+ * @author asier.ruiz
+ */
+public class EjDelMartesGrafo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/es/medac/ejdelmartesgrafo/Graph.java
@@ -0,0 +1,11 @@
+package es.medac.ejdelmartesgrafo;
+
+/**
+ *
+ * @author asier.ruiz
+ */
+public abstract class Graph {
+    public abstract void addVertex(String l1);
+    public abstract void addEdge(String l1, String l2);
+    public abstract void printGraph();
+}
