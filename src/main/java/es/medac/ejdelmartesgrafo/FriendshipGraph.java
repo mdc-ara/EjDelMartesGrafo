105c932c032694a6465831783eb0388024c0717b
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-02-15 08:28:03 +0100

    Implementación con Collections de la clase concreta FriendshipGraph

--- src/main/java/es/medac/ejdelmartesgrafo/FriendshipGraph.java
+++ src/main/java/es/medac/ejdelmartesgrafo/FriendshipGraph.java
@@ -4,5 +4,33 @@ package es.medac.ejdelmartesgrafo;
  *
  * @author asier.ruiz
  */
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FriendshipGraph extends Graph {
+    private HashMap<String, List<String>> adjVertices;
+    
+    public FriendshipGraph() {
+        adjVertices = new HashMap<>();
+    }
+
+    @Override
+    public void addVertex(String l1) {
+        adjVertices.putIfAbsent(l1, new ArrayList<>());
+    }
+
+    @Override
+    public void addEdge(String l1, String l2) {
+        adjVertices.get(l1).add(l2);
+        adjVertices.get(l2).add(l1);
+    }
+
+    @Override
+    public void printGraph() {
+        for(String vertice: adjVertices.keySet()){
+            // Las listas implementan toString
+            System.out.println(vertice + " tiene amistad con: "+ adjVertices.get(vertice));
+        }
+    }
 }
